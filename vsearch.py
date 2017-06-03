1321fce14669417b1a46365172ef1e1bd654f1b4
hujingyin <hujingyin@users.noreply.github.com>
2017-06-03 21:06:53 +0800

    Delete vsearch.py

--- vsearch.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import csv
-import requests
-with open ('routine.csv',encoding='utf8')as raw_data:
-    for line in csv.DictReader(raw_data):
-        print(line)
-       
